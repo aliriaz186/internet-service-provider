30d19c061fb17bbeace5d83ee8b164450fed8473
Ali <me.aliriaz007@gmail.com>
2021-04-22 20:37:26 +0500

    commented

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -8,19 +8,19 @@
 
     <link rel="stylesheet" href="{{url('')}}/assets/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/animate.min.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/animate.min.css">--}}
 
     <link rel="stylesheet" href="{{url('')}}/assets/css/fontawesome.min.css">
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/magnific-popup.min.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/magnific-popup.min.css">--}}
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/flaticon.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/flaticon.css">--}}
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/nice-select.min.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/nice-select.min.css">--}}
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/meanmenu.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/meanmenu.css">--}}
 
-    <link rel="stylesheet" href="{{url('')}}/assets/css/owl.carousel.min.css">
+{{--    <link rel="stylesheet" href="{{url('')}}/assets/css/owl.carousel.min.css">--}}
 
     <link rel="stylesheet" href="{{url('')}}/assets/css/style.css">
 
@@ -159,25 +159,25 @@
 
 <script src="{{url('')}}/assets/js/bootstrap.min.js"></script>
 
-<script src="{{url('')}}/assets/js/jquery.meanmenu.js"></script>
+{{--<script src="{{url('')}}/assets/js/jquery.meanmenu.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/jquery.magnific-popup.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/jquery.magnific-popup.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/owl.carousel.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/owl.carousel.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/parallax.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/parallax.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/jquery.nice-select.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/jquery.nice-select.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/wow.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/wow.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/jquery.ajaxchimp.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/jquery.ajaxchimp.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/form-validator.min.js"></script>
+{{--<script src="{{url('')}}/assets/js/form-validator.min.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/contact-form-script.js"></script>
+{{--<script src="{{url('')}}/assets/js/contact-form-script.js"></script>--}}
 
-<script src="{{url('')}}/assets/js/bahama-map.js"></script>
+{{--<script src="{{url('')}}/assets/js/bahama-map.js"></script>--}}
 
 <script src="{{url('')}}/assets/js/main.js"></script>
 </body>
